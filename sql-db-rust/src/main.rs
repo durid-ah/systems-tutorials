87c0745083012bccbbacd0db936ed1b6c252eede
Durid Ahmad <ahmad.durid96@gmail.com>
2021-02-18 23:31:08 -0700

    started writing the parsing statements and the related structs and enums

--- sql-db-rust/src/main.rs
+++ sql-db-rust/src/main.rs
@@ -1,7 +1,31 @@
 use std::io::{stdin, stdout, Write};
 use std::process;
 
-fn parse_input(user_input: &String) {
+enum StatementType {
+    InsertStatement,
+    SelectStatement
+}
+
+struct Statement {
+    st_type: StatementType 
+}
+
+fn prepare_statement(command: &String, statement: &mut Statement) -> Result<(), String> {
+    let operation = &command[0..6];
+    match operation {
+        "select" => {
+            statement.st_type = StatementType::SelectStatement;
+            return Ok(())
+        },
+        "insert" => {
+            statement.st_type = StatementType::InsertStatement;
+            return Ok(())
+        },
+        _ => return Err(String::from("Unrecognized Statement"))
+    }
+}
+
+fn parse_meta_command(user_input: &String) {
     let input = user_input.trim_end();
     match input {
         ".exit" => process::exit(0),
@@ -12,15 +36,16 @@ fn parse_input(user_input: &String) {
 fn main() {
     loop {
         let mut input = String::new();
-        print!("db >");
+        print!("db > ");
         let _ = stdout().flush();
         stdin()
             .read_line(&mut input)
             .expect("An error occured reading user input");
         let first_char = input.chars().next().unwrap();
-        
+
         if first_char == '.' {
-            parse_input(&input);
+            parse_meta_command(&input);
+            continue;
         }
     }
     
