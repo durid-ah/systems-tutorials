9b5a7c59bfbf1e5cc7764e88412ea0d4cea82a5d
Durid Ahmad <ahmad.durid96@gmail.com>
2021-02-18 19:56:18 -0700

    sql-db-rust: initial REPL

--- sql-db-rust/src/main.rs
+++ sql-db-rust/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{stdin, stdout, Write};
+use std::process;
+
+fn parse_input(user_input: &String) {
+    let input = user_input.trim_end();
+    match input {
+        ".exit" => process::exit(0),
+        _ => println!("Unrecognized Command '{}'", input)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        print!("db >");
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut input)
+            .expect("An error occured reading user input");
+        let first_char = input.chars().next().unwrap();
+        
+        if first_char == '.' {
+            parse_input(&input);
+        }
+    }
+    
 }
