1f4e64d264e540759d268bd18723003fa7c7dd33
durid-ah <ahmad.durid96@gmail.com>
2021-10-14 20:00:32 -0600

    adding file read and file seek to the open pager function

--- sql-db-rust/src/pager.rs
+++ sql-db-rust/src/pager.rs
@@ -1,7 +1,37 @@
-pub struct Pager {
+use std::fs::{File, OpenOptions};
+use std::io::{Seek, SeekFrom};
 
+pub struct Pager {
+   pub file: File,
+   pub file_length: u64
 }
 
 impl Pager {
+   pub fn open_pager(file_name: String) -> Option<Pager> {
+      let mut _file = OpenOptions::new()
+         .read(true)
+         .write(true)
+         .create(true)
+         .open(file_name)
+         .unwrap();
+
+      println!("{:?}", _file);
+      let _size = _file.seek(SeekFrom::End(0)).unwrap();
+      println!("{:?}", _size);
+
+      
    
+      Option::None
+   }
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+   
+   #[test]
+   fn constructor_test() {
+      let pager  = Pager::open_pager(String::from("test_file.txt"));
+      assert_eq!(true, true)
+   }
 }
