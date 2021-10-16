f73ec8c918592493ae31fa95ec7aab680fb3193c
durid-ah <ahmad.durid96@gmail.com>
2021-10-16 13:40:28 -0600

    esthetic changes to the Pager

--- sql-db-rust/src/pager.rs
+++ sql-db-rust/src/pager.rs
@@ -7,10 +7,13 @@ use super::size_constants::{
    TABLE_MAX_PAGES,
 };
 
+type Page = [Option<Vec<u8>>; ROWS_PER_PAGE];
+type UninitPage = [MaybeUninit<Option<Vec<u8>>>; ROWS_PER_PAGE];
+
 pub struct Pager {
    pub file: File,
    pub file_length: u64,
-   pub pages: [Option<[Option<Vec<u8>>; ROWS_PER_PAGE]>; TABLE_MAX_PAGES]
+   pub pages: [Option<Page>; TABLE_MAX_PAGES]
 }
 
 impl Pager {
@@ -28,9 +31,10 @@ impl Pager {
       return Pager {file: _file, file_length: _size, pages: _pages}
    }
 
-   fn init_pages() -> [Option<[Option<Vec<u8>>; ROWS_PER_PAGE]>; TABLE_MAX_PAGES] {
+   /// Initialize each page to Option::None
+   fn init_pages() -> [Option<Page>; TABLE_MAX_PAGES] {
       return {
-         let mut _pages: [MaybeUninit<Option<[Option<Vec<u8>>; ROWS_PER_PAGE]>>; TABLE_MAX_PAGES] = unsafe {
+         let mut _pages: [MaybeUninit<Option<Page>>; TABLE_MAX_PAGES] = unsafe {
             // the compiler assumes the array is initialized when it isn't
             MaybeUninit::uninit().assume_init()
          };
@@ -41,7 +45,7 @@ impl Pager {
          }
 
          //remove the MaybeUninit part of the type to make it a an option array
-         unsafe { mem::transmute::<_, [Option<[Option<Vec<u8>>; ROWS_PER_PAGE]>; TABLE_MAX_PAGES]>(_pages)}
+         unsafe { mem::transmute::<_, [Option<Page>; TABLE_MAX_PAGES]>(_pages)}
       };
    }
 
@@ -56,8 +60,8 @@ impl Pager {
    
       let page = &mut self.pages[(page_num as usize)];
       if let Option::None =  page {
-         let mut _page: [Option<Vec<u8>>; ROWS_PER_PAGE] = {
-            let mut _init_page : [MaybeUninit<Option<Vec<u8>>>; ROWS_PER_PAGE] = unsafe {
+         let mut _page: Page = {
+            let mut _init_page : UninitPage = unsafe {
                MaybeUninit::uninit().assume_init()
             };
 
@@ -73,5 +77,21 @@ impl Pager {
 
       let res = self.pages[(page_num as usize)].as_mut().unwrap();
       &mut res[row_idx]
+   }
+
+   /// Init the rows of each page to Option::None
+   pub fn init_page_rows(&mut self, page: &mut Option<Page>) {
+      let mut _page: Page = {
+         let mut _init_page : UninitPage = unsafe {
+            MaybeUninit::uninit().assume_init()
+         };
+         for r in &mut _init_page[..] {
+            *r = MaybeUninit::new(Option::None);
+         }
+         
+         unsafe {mem::transmute(_init_page)}
+      };
+
+      *page = Option::Some(_page);
    }
 }
