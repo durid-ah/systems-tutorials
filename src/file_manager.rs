f54d136cfdb655f4d9c07ce9a698cadb912d041b
durid-ah <ahmad.durid96@gmail.com>
2021-11-09 20:12:49 -0700

    modified the read size method to return zero at None values

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -3,6 +3,7 @@ use std::iter::Peekable;
 use std::io::{Seek, SeekFrom, Read, Write, IoSlice, Bytes};
 use std::borrow::BorrowMut;
 
+use super::size_constants::PAGE_SIZE;
 
 pub struct FileManager {
    file: File,
@@ -12,11 +13,11 @@ pub struct FileManager {
 impl FileManager {
    pub fn new(path: String) -> FileManager {
       let mut file = OpenOptions::new()
-      .read(true)
-       .write(true)
-      .create(true)
-      .open(path)
-      .unwrap();
+         .read(true)
+         .write(true)
+         .create(true)
+         .open(path)
+         .unwrap();
 
       let file_length = file.seek(SeekFrom::End(0)).unwrap();
       return FileManager {file, file_length}
@@ -25,18 +26,48 @@ impl FileManager {
    fn read_row_size_header(iter: &mut Peekable<Bytes<&mut File>>) -> u16{
       let mut size_buffer :[u8; 2] = [0; 2];
       size_buffer[0] = iter.next()
-         .expect("Unable to read byte 1 of size header")
-         .unwrap();
+         .unwrap_or(Ok(0))
+         .unwrap_or(0);
 
       size_buffer[1] = iter.next()
-         .expect("Unable to read byte 2 of size header")
-         .unwrap();
+         .unwrap_or(Ok(0))
+         .unwrap_or(0);
 
       return u16::from_le_bytes(size_buffer);
    }
 
-   // TODO: Add a seek page by number function
-   
+   pub fn seek_to_page(&mut self, page_num: u64) {
+      let offset_bytes = page_num * PAGE_SIZE;
+      let offset = SeekFrom::Start(offset_bytes);
+      
+      self.file.seek(offset).expect("unable to seek to page");
+   }
+
+   pub fn read_row(&mut self) -> Option<Vec<u8>> {
+      let mut file_iter = self.file.borrow_mut()
+         .bytes()
+         .peekable();
+
+      let row_size = FileManager::read_row_size_header(&mut file_iter);
+
+      if row_size <= 0 {
+         return None;
+      }
+
+      let row: Vec<u8> = Vec::with_capacity(row_size as usize);
+      
+      for i in 0..row_size {
+         let file_byte = file_iter.next()
+            .expect(
+               format!("FILE CORRUPTION: Invalid number of row bytes expected {} got {}", row_size, i)
+            ).unwrap();
+
+         row.push(file_byte)
+      }
+
+      Some(row)
+   }
+
    // TODO: Convert the code below to load a single row (load_next_row()) ?
    // TODO: The row might be a None if end of file reached
    // TODO: What will the end of page implications be?
