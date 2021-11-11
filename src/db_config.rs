2bee86e9f5416bd9d14e0fa4f748310133245d75
durid-ah <ahmad.durid96@gmail.com>
2021-11-10 23:53:08 -0700

    added db config methods to load and save json

--- src/db_config.rs
+++ src/db_config.rs
@@ -1,3 +1,40 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs::{File, OpenOptions};
+use std::io::{Write, Read};
+
+#[derive(Serialize, Deserialize)]
 pub struct DBConfig {
    pub num_rows: u16
+}
+
+impl DBConfig {
+   pub fn load() -> DBConfig {
+      let mut config_str = String::new();
+
+      let _ = OpenOptions::new()
+         .read(true)
+         .open("dbConfig.json")
+         .unwrap()
+         .read_to_string(&mut config_str);
+
+      println!("{:?}", config_str);
+ 
+      if config_str.len() == 0 {
+         return DBConfig{num_rows: 0}
+      }
+
+      return serde_json::from_str(config_str.as_str())
+         .unwrap();
+   }
+
+   pub fn save(config: DBConfig) {
+      let json = serde_json::to_string(&config).unwrap();
+
+      let _ = OpenOptions::new()
+         .write(true)
+         .open("dbConfig.json")
+         .unwrap()
+         .write_all(json.as_bytes());
+   }
 }
